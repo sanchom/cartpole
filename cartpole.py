2790033d3f38386231c4469c0df4ca320a82c1a5
Sancho McCann <sanchom@gmail.com>
2017-03-15 14:56:19 -0700

    Added discretization factory.

--- cartpole.py
+++ cartpole.py
@@ -10,9 +10,17 @@ import numpy as np
 import gym
 from gym import wrappers
 
+class StateBinner:
+  @classmethod
+  def get_binner(classname, environment_name, env):
+    if environment_name == 'CartPole-v0':
+      return CartpoleStateBinner(env)
+    if environment_name == 'Acrobot-v1':
+      return AcrobotStateBinner(env)
+
 # State-space taken from Matthew Chan
 # (https://medium.com/@tuzzer/cart-pole-balancing-with-q-learning-b54c6068d947)
-class StateBinner(object):
+class CartpoleStateBinner(object):
   def __init__(self, env):
     self.state_bounds = list(zip(env.observation_space.low, env.observation_space.high))
     # The bounds for this dimension (x_dot) are too broad (basically
@@ -25,6 +33,26 @@ class StateBinner(object):
 
   def discretize(self, observation):
     discrete = [0, 0, 0, 0]
+    for i, val in enumerate(observation):
+      low, high = self.state_bounds[i]
+      if val < low:
+        discrete[i] = 0
+      elif val > high:
+        discrete[i] = self.state_bins[i] + 1
+      else:
+        relative_val = (val - low)
+        bin_size = (high - low) / self.state_bins[i]
+        b = int(relative_val / bin_size)
+        discrete[i] = b + 1
+    return tuple(discrete)
+
+class AcrobotStateBinner(object):
+  def __init__(self, env):
+    self.state_bounds = list(zip(env.observation_space.low, env.observation_space.high))
+    self.state_bins = [5, 5, 5, 5, 10, 10]
+
+  def discretize(self, observation):
+    discrete = [0, 0, 0, 0, 0, 0]
     for i, val in enumerate(observation):
       low, high = self.state_bounds[i]
       if val < low:
@@ -62,7 +90,7 @@ class QLearnerAgent(object):
     except KeyError:
       best_q_forward = 0.0
 
-    discount_factor = 0.99
+    discount_factor = 0.9
     try:
       original_q = self.q[starting_state][action]
     except KeyError:
@@ -76,11 +104,12 @@ class QLearnerAgent(object):
       self.q[starting_state][action] = new_q
 
 def main():
-  env = gym.make('CartPole-v0')
+  env_name = 'Acrobot-v1'
+  env = gym.make(env_name)
   #env = wrappers.Monitor(env, '/tmp/cartpole_experiment')
   agent = QLearnerAgent(env)
-  binner = StateBinner(env)
-  for episode in xrange(1000):
+  binner = StateBinner.get_binner(env_name, env)
+  for episode in xrange(100000):
     observation = env.reset()
     state = binner.discretize(observation)
     # Learning rate decay and exploration rate decay taken from
@@ -89,15 +118,17 @@ def main():
     learning_rate = max(0.1, min(0.5, 1.0 - math.log10((episode + 1) / 25)))
     explore_rate = max(0.01, min(1, 1.0 - math.log10((episode + 1) / 25)))
     env.render()
+    cumulative_reward = 0
     for i in xrange(1000):
       action = agent.get_action(state, explore_rate)
       observation, reward, done, _ = env.step(action)
+      cumulative_reward += reward
       new_state = binner.discretize(observation)
       agent.learn(state, action, reward, new_state, learning_rate)
       state = new_state
       env.render()
       if done:
-        print('Episode {} finished after {} steps.'.format(episode + 1, i + 1))
+        print('Episode {} finished after {} steps. Reward = {}'.format(episode + 1, i + 1, cumulative_reward))
         break
 
 if __name__ == '__main__':
